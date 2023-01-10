582bb28e5c2a633586a2b16c26bdc02d316dbf4b
Xiang <xiangqingkou@163.com>
2023-01-11 00:15:16 +0800

    second commit

--- src/main/java/com/Colson/GitTest.java
+++ src/main/java/com/Colson/GitTest.java
@@ -3,5 +3,6 @@ package com.Colson;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GIt");
+        System.out.println("Hello GIt2");
     }
 }
