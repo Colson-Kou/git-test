1826c8d1cf995b6fb7f9bae87995f16f37df13eb
Xiang <xiangqingkou@163.com>
2023-01-11 15:37:07 +0800

    master commit2

--- src/main/java/com/Colson/GitTest.java
+++ src/main/java/com/Colson/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hello GIt");
         System.out.println("Hello GIt2");
         System.out.println("Hello GIt34  ");
+        System.out.println("master test  ");
 
     }
 }
